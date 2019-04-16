9bb80f5ec13678c8d0a65225d23d70bc6ac4bc05
btronquoy <b.tronquoy@gmail.com>
2019-04-16 09:25:27 +0200

    fix(Search): add exeptions to the search function

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+# for test only
+testMode = False
+
 current_path = config.current_path
+print('current path ===' + current_path)
 
 # Service account (config here: https://console.developers.google.com/projectselector2/iam-admin/serviceaccounts?pli=1&supportedpurview=project&project&folder&organizationId)
 serviceAccountConfig = credentials.Certificate(current_path + '/serviceAccountConfig.json')
@@ -20,18 +24,25 @@ posts = db.collection(u'posts')
 reddit = praw.Reddit(user_agent=config.data['user_agent'],
                      client_id=config.data['client_id'], client_secret=config.data['client_secret'],
                      username=config.data['username'], password=config.data['password'])
+
 # discord webhook
 hook = Webhook(config.data['discord_webhook'])
+hook_septra = Webhook(config.data['discord_webhook_septra'])
 
+# img to illustrate Discord post
 imgBig = 'https://i.imgur.com/9sv9ZUu.jpg'
 imgIcon = 'https://i.imgur.com/oNg2zU2.png'
 
-for submission in reddit.subreddit('starcitizen').search('title:patch note', sort='new', time_filter='week'):
-        print(submission.created)
-        print(submission.title)
-        postId = submission.id
-        postCreated = str(submission.created)
-        postTitle = str(submission.title)
+for submission in reddit.subreddit('starcitizen').search('title:(patch note) OR (star citizen alpha out)', sort='new', time_filter='week'):
+
+    print(submission.created)
+    print(submission.title)
+    postId = submission.id
+    postCreated = str(submission.created)
+    postTitle = str(submission.title)
+
+    # if the post links to spectrum
+    if 'https://robertsspaceindustries.com/spectrum/' in submission.url:
 
         if posts.document(u''+ postCreated + '').get().exists == False:
             doc_ref = posts.document(u'' + postCreated + '')
@@ -49,7 +60,16 @@ for submission in reddit.subreddit('starcitizen').search('title:patch note', sor
             embed.add_field(name='Info Patch', value=''+ postTitle  +'')
             embed.set_footer(text='Script by Luicid', icon_url=imgIcon)
             embed.set_image(imgBig)
-            hook.send(embed=embed)
-            hook.send("@everyone")
+
+            if testMode == False:
+                hook.send(embed=embed)
+                hook_septra.send(embed=embed)
+                hook.send("@everyone")
+            else:
+                print('Test Mode enabled - don\'t send discord msg')
+
+
         else:
             print('The document: ' + postCreated + ' for the post ' + postId + ' exist in the database')
+    else:
+        print('Document with title "' + submission.title + '" is ignored as is not a spectrum post)')
